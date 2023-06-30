04656b33734a9e6e9d84b6bf9a18b856c36f986d
king-louis9 <kingsley6294@gmail.com>
2023-06-30 08:24:55 -0700

    Variables if else while commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Description: 'Prints all single digit numbers of base10'
+ * Description: Prints all single digit numbers of base10
  * Return: Always 0(Success)
  */
+
 int main(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
-		printf("%d", n);
+	printf("%d", n);
 	putchar('\n');
 	return (0);
 }
