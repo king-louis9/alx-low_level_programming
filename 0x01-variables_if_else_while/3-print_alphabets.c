efbe2a06dcfc6805c1b4f6f030e46d1d1da035c2
king-louis9 <kingsley6294@gmail.com>
2023-06-30 08:21:20 -0700

    Variables if else while commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,13 @@
 int main(void)
 
 {
-	int ch;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	putchar(ch);
+	for (i = 0; i < 52; i++)
+	{
+	putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
